d96c787e8f355437d81e3fb1e82ec2bb234ec412
Ethan Jimenez <erosethanjv@gmail.com>
2015-08-06 21:46:13 -0500

    grafo.cpp editado por Ethan

--- grafo.cpp
+++ grafo.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Definiciones iniciales
+typedef int Arista;
+typedef vector<Arista> Nodo;
+typedef pair<int, int> AristaPeso;
+typedef vector<AristaPeso> NodoPeso;
+
+const int INF (1 << 30);
 const int MAXN = 100000;
 const int MAXM = 100000;
 
-typedef int Arista;
-typedef pair<int, int> AristaPeso;
-typedef vector<Arista> Nodo;
-
 Nodo grafo[MAXN];
-
+Nodo grafo_peso[MAXN];
 int in_degree[MAXN];
 
 void AgregarArista(int u, int v) {
-	grafo[u].push_back(v);
-	++in_degree[v];
+    grafo[u].push_back(v);
+    ++in_degree[v];
 }
+
+// Detecta ciclos en una componente conexa bidireccional.
+// Devuelve -1 si no existen ciclos. En caso de existir,
+// los nodos del ciclo se guardan en el arreglo ciclo.
 
 int top_ciclo;
 int ciclo[MAXN];
@@ -23,117 +30,116 @@ bool ciclo_activo;
 bool visitado[MAXN];
 
 int EncontrarCiclo_(int u, int p) {
-	visitado[u] = true;
-	for(int i = 0; i < grafo[u].size(); ++i) {
-		int v = grafo[u][i];
-		if (v == p) continue;
-		if (visitado[v]) return ciclo[top_ciclo++] = v; 
-		if ((int cic = EncontrarCiclo_(v, u)) >= 0) {
-			if (ciclo_activo) ciclo[top_ciclo++] = v;
-			if (cic == u) ciclo_activo = false;
-			return cic;
-		}
-	}
-	return -1;
+    visitado[u] = true;
+    for (int i = 0; i < grafo[u].size(); ++i) {
+        if (grafo[u][i] == p) continue;
+        int v = grafo[u][i], cic;
+        if (visitado[v]) return ciclo[top_ciclo++] = v; 
+        if ((cic = EncontrarCiclo_(v, u)) >= 0) {
+            if (ciclo_activo) ciclo[top_ciclo++] = v;
+            if (cic == u) ciclo_activo = false;
+            return cic;
+        }
+    }
+    return -1;
 }
 
-int EncontrarCiclo(int u){
-	ciclo_activo = true, top_ciclo = 0;
-	return EncontrarCiclo_(u, -1);
+int EncontrarCiclo(int u) {
+    ciclo_activo = true, top_ciclo = 0;
+    return EncontrarCiclo_(u, -1);
 }
 
-//menor a mayor dependencia
-//indexado de 0 a n-1
-vector <int> ordentopo;
+// Orden topologico de un grafo dirigido aciclico.
+// Los indices de los nodos se asumen de 0 a n - 1.
+
+vector<int> orden_topo; // Resultado
 
 void OrdenTopologico_(int u) {
-	visitado[u] = true;
-	for(int i = 0; i < grafo[u].size(); ++i) {
-		int v =  grafo[u][i];
-		if(!visitado[v])
-			OrdenTopologico_(v);
-	}
-	ordentopo.push_back(u);
-}
-void OrdenTopologico(int n){
-	fill(visitado, visitado+n, false);
-	ordentopo.clear();
-	for(int i = 0; i < n; i++) {
-		if(!visitado[i])
-			OrdenTopologico_(i);
-	}
+    visitado[u] = true;
+    for (int i = 0; i < grafo[u].size(); ++i)
+        if (!visitado[grafo[u][i]])
+            OrdenTopologico_(grafo[u][i]);
+    orden_topo.push_back(u);
 }
 
-int numeracion;
-int top_activo;
-int low[MAXN];
-int num[MAXN];
-int activos[MAXN];
-
-void ObtenerCFC_(int u, vector <vector<int> >& componentes) {
-	low[u]=num[u]=++numeracion;
-	activo[top_activo++] = u;
-	for(int i = 0; i < grafo[u].size(); ++i) {
-		int v = grafo[u][i];
-		if(!num[v])
-			ObtenerCFC_(v, componentes);
-		low[u] = min(low[u], low[v]);
-	}
-	if(low[u] == num[u]) {
-		vector<int> cFC;
-		while( activo[top_activo - 1] != u ) {
-			cFC.push_back(activo[--top_activo]);
-			low[activo[top_activo]] = INF;
-		}
-		cFC.push_back(activo[--top_activo]);
-		low[activo[u]] = INF;
-		componentes.push_back(cFC);
-	}
-}
-vector <vector<int> > ObternerCFC(int n) {
-	vector <vector<int> > cfcs;
-	fill(num, num + n, 0);
-	fill(low, low + n, 0);
-	numeracion = 0;
-	for(int i = 0; i < n; ++i) {
-		if(!num[i]) ObternerCFC_(i, cfcs);
-	}
-	return cfcs;
+void OrdenTopologico(int n) {
+    orden_topo.clear();
+    fill(visitado, visitado + n, false);
+    for (int i = 0; i < n; ++i)
+        if (!visitado[i]) OrdenTopologico_(i);
 }
 
-Nodo puentes[MAXN];
-bool punto_art[MAXN];
+// Obtener componentes fuertemente conexas en un grafo
+// dirigido. Asume los nodos indexados de 0 a n - 1.
 
-void PuntosArtPuntes_(int u, int p) {
- 	low[u] = num[u] = ++numeracion;
- 	for(int i = 0; i < grafo[u].size(); i++) {
- 		int v = grafo[u][i];
- 		if(v == p) continue;
- 		if(!num[v]) PuntosArtPuntes_(v, u);
- 		if(low[v] > num[u]) {
- 			puentes[u].push_back(v);
- 			puentes[v].push_back(u);
- 		}
- 		punto_art[u] |=low[v] >= num[u];
- 		low[u] = min(low[u], low[v]);
- 	}
- 	low[u] = INF;
+int numeracion, top_activo;
+int low[MAXN], num[MAXN], activo[MAXN];
+vector< vector<int> > CFCs; // Resultado
+
+void ObtenerCFCs_(int u) {
+    activo[top_activo++] = u;
+    low[u] = num[u] = ++numeracion;
+    for (int i = 0; i < grafo[u].size(); ++i) {
+        int v = grafo[u][i];
+        if (!num[v]) ObtenerCFCs_(v);
+        low[u] = min(low[u], low[v]);
+    }
+    if (low[u] == num[u]) {
+        vector<int> CFC;
+        while (activo[top_activo - 1] != u) {
+            CFC.push_back(activo[--top_activo]);
+            low[activo[top_activo]] = MAXN;
+        }
+        CFC.push_back(activo[--top_activo]);
+        low[u] = INF; CFCs.push_back(CFC);
+    }
 }
 
-void PuntosArtPuntes(int n) {
-	fill(num, num + n, 0);
-	fill(low, low + n, 0);
-	numeracion = 0;
-	fill(punto_art, punto_art + n, false);
-	for(int i = 0; i < n; i++) puentes[i].clear();
-	for(int i = 0; i < n; i++) {
-		if(num[i]) continue;
-		PuntosArtPuntes_(i, -1);
-		punto_art[i]= grafo[i].size() > 1;
-	}
+void ObtenerCFCs(int n) {
+    CFCs.clear();
+    numeracion = 0;
+    fill(num, num + n, 0);
+    fill(low, low + n, 0);
+    for (int i = 0; i < n; ++i)
+        if (!num[i]) ObtenerCFCs_(i);
 }
 
+// Detecta los puentes y puntos de articulacion en
+// un grafo bidireccional. Indices de 0 a n - 1.
+
+Nodo puentes[MAXN]; // Resultado
+bool punto_art[MAXN]; // Resultado
+
+void PuntosArtPuentes_(int u, int p) {
+    low[u] = num[u] = ++numeracion;
+    for (int i = 0; i < grafo[u].size(); ++i) {
+        if (grafo[u][i] == p) continue;
+        int v = grafo[u][i];
+        if (!num[v]) PuntosArtPuentes_(v, u);
+        if (low[v] > num[u]) {
+            puentes[u].push_back(v);
+            puentes[v].push_back(u);
+        }
+        punto_art[u] |= low[v] >= num[u];
+        low[u] = min(low[u], low[v]);
+    }
+    low[u] = INF;
+}
+
+void PuntosArtPuentes(int n) {
+    numeracion = 0;
+    fill(num, num + n, 0);
+    fill(low, low + n, 0);
+    fill(punto_art, punto_art + n, false);
+    for(int i = 0; i < n; ++i)
+        puentes[i].clear();
+    for(int i = 0; i < n; ++i) {
+        if(num[i]) continue;
+        PuntosArtPuentes_(i, -1);
+        punto_art[i] = grafo[i].size() > 1;
+    }
+}
 
 int main() {
-	return 0;
+    return 0;
 }

9d91bd3ee34fd22d57c185dd60c945ebc8e8c9cc
gamalaurito <ej_sanchezg@live.com>
2015-08-06 20:15:35 -0500

    Grafos componentes fuertemente conexas, puentes y nodos articulados

--- grafo.cpp
+++ grafo.cpp
@@ -64,6 +64,76 @@ void OrdenTopologico(int n){
 	}
 }
 
+int numeracion;
+int top_activo;
+int low[MAXN];
+int num[MAXN];
+int activos[MAXN];
+
+void ObtenerCFC_(int u, vector <vector<int> >& componentes) {
+	low[u]=num[u]=++numeracion;
+	activo[top_activo++] = u;
+	for(int i = 0; i < grafo[u].size(); ++i) {
+		int v = grafo[u][i];
+		if(!num[v])
+			ObtenerCFC_(v, componentes);
+		low[u] = min(low[u], low[v]);
+	}
+	if(low[u] == num[u]) {
+		vector<int> cFC;
+		while( activo[top_activo - 1] != u ) {
+			cFC.push_back(activo[--top_activo]);
+			low[activo[top_activo]] = INF;
+		}
+		cFC.push_back(activo[--top_activo]);
+		low[activo[u]] = INF;
+		componentes.push_back(cFC);
+	}
+}
+vector <vector<int> > ObternerCFC(int n) {
+	vector <vector<int> > cfcs;
+	fill(num, num + n, 0);
+	fill(low, low + n, 0);
+	numeracion = 0;
+	for(int i = 0; i < n; ++i) {
+		if(!num[i]) ObternerCFC_(i, cfcs);
+	}
+	return cfcs;
+}
+
+Nodo puentes[MAXN];
+bool punto_art[MAXN];
+
+void PuntosArtPuntes_(int u, int p) {
+ 	low[u] = num[u] = ++numeracion;
+ 	for(int i = 0; i < grafo[u].size(); i++) {
+ 		int v = grafo[u][i];
+ 		if(v == p) continue;
+ 		if(!num[v]) PuntosArtPuntes_(v, u);
+ 		if(low[v] > num[u]) {
+ 			puentes[u].push_back(v);
+ 			puentes[v].push_back(u);
+ 		}
+ 		punto_art[u] |=low[v] >= num[u];
+ 		low[u] = min(low[u], low[v]);
+ 	}
+ 	low[u] = INF;
+}
+
+void PuntosArtPuntes(int n) {
+	fill(num, num + n, 0);
+	fill(low, low + n, 0);
+	numeracion = 0;
+	fill(punto_art, punto_art + n, false);
+	for(int i = 0; i < n; i++) puentes[i].clear();
+	for(int i = 0; i < n; i++) {
+		if(num[i]) continue;
+		PuntosArtPuntes_(i, -1);
+		punto_art[i]= grafo[i].size() > 1;
+	}
+}
+
+
 int main() {
 	return 0;
 }

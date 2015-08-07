cc11e1f8b958a936e192fe63242b4480de5eefa1
gamalaurito <ej_sanchezg@live.com>
2015-08-07 14:22:33 -0500

    Estructura Union Find

--- grafo.cpp
+++ grafo.cpp
@@ -140,6 +140,41 @@ void PuntosArtPuentes(int n) {
     }
 }
 
+
+//union find estructura
+struct UnionFind {
+    int nconjuntos;
+    vector<int> padre;
+    vector<int> tamano;
+
+    UnionFind(int n): nconjuntos(n), padre(n), tamano(n, 1) {
+        for(int i = 0; i < n; i++) {
+            padre[i] = i;
+        }
+    }
+
+   int Encontrar(int u) {
+        if(padre[u] == u) return u;
+        return padre[u] = Encontrar(padre[u]);
+    }
+
+    void Unir(int u, int v) {
+        int Ru = Encontrar(u);
+        int Rv = Encontrar(v);
+        if (Ru == Rv) return;
+        -- nconjuntos;
+        padre[Ru] = Rv;
+        tamano[Rv] += tamano[Ru];
+    }
+
+    bool MismoConjunto(int u, int v) {
+        return Encontrar(u) == Encontrar(v);
+    }
+    int TamanoConjunto (int u) {
+        return tamano[Encontrar(u)];
+    }
+};
+
 int main() {
     return 0;
 }

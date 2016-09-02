a20c550d0f3f066ce411531ca192669c84eaf296
Ethan Jimenez <erosethanjv@gmail.com>
2016-09-02 12:01:04 -0500

    Agregado prueba de grafo bipartito

--- grafo.cpp
+++ grafo.cpp
@@ -141,6 +141,29 @@ struct Grafo {
         for (int u = 0; u < n; ++u)
             if (!label[u]) FuertementeConexo(u);
     }
+    
+    // Determina si un grafo bidireccional
+    // es bipartito (o bien, es bicoloreable).
+
+    bool EsBipartito() {
+        vector<char> color(n, -1);
+        for (int u = 0; u < n; ++u) {
+            if (color[u] >= 0) continue;
+
+            color[u] = 0;
+            queue<int> q; q.push(u);
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                for (int v : ady[u]) {
+                    if (color[v] < 0) q.push(v),
+                        color[v] = 1 - color[u];
+                    if (color[u] == color[v])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
 
     // REVISIT
     // Obtiene el orden topologico de los nodos
